86d52bc61448852e36510f5ff6da39f7fcd2f585
robson <robson@192.168.0.107>
2018-08-23 21:29:09 -0300

    commit local

---
+++ proj-geral/src/br/mac/projgeral/main/Main.java
@@ -0,0 +1,13 @@
+package br.mac.projgeral.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		hellow();
+
+	}
+
+	public static void hellow() {
+		System.out.println("Hello Mac...");
+	}
+}
